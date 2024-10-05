f8165ffcb2933504ac5adb9eff692d059d3131b6
Ivan Grigorev <iv.st.grigorev@gmail.com>
2024-10-06 00:49:27 +0200

    Set up Vue instance and plugins in main.js

--- ai_model_management/frontend/src/main.js
+++ ai_model_management/frontend/src/main.js
@@ -1,7 +1,16 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import axios from 'axios';
 
 import 'flowbite';
 import './assets/tailwind.css';
+import router from './router';
 
-createApp(App).mount('#app')
+// create the Vue app
+const app = createApp(App);
+
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = 'http://localhost:8000/';  // the FastAPI backend
+
+app.use(router) // Register the router
+app.mount('#app'); // Mount the app to the DOM
