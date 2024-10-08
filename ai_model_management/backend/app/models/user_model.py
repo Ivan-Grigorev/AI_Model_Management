9106dd9c45e3938f063038f82e7b5e2a8443eceb
Ivan Grigorev <iv.st.grigorev@gmail.com>
2024-10-08 13:34:54 +0200

    Add user registration date

--- ai_model_management/backend/app/models/user_model.py
+++ ai_model_management/backend/app/models/user_model.py
@@ -1,6 +1,6 @@
 """User model for storing user data in the database."""
 
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Boolean
 
 from ..core.database import Base
 
@@ -13,6 +13,7 @@ class User(Base):
         id (int): Unique identifier for the user.
         email (str): User's email address (used for login).
         hashed_password (str): User's hashed password for authentication.
+        registration_date (str): The User's registration date.
     """
 
     __tablename__ = 'users'
@@ -20,3 +21,4 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
+    registration_date = Column(String)
