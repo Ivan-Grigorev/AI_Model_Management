cb2f28dfae1e1f13af21d08de5dd66ef376c8e5e
Ivan Grigorev <iv.st.grigorev@gmail.com>
2024-10-08 08:32:22 +0200

    Add date field for timestamping entries

--- ai_model_management/frontend/src/views/ModelDetailsView.vue
+++ ai_model_management/frontend/src/views/ModelDetailsView.vue
@@ -5,6 +5,7 @@
     <div class="border p-4 rounded-lg bg-gray-100">
       <p><strong>ID:</strong> {{ model.id }}</p>
       <p><strong>Name:</strong> {{ model.name }}</p>
+      <p><strong>Creation Date:</strong> {{ model.creation_date }}</p>
     </div>
 
     <button @click="$router.push('/models')" class="mt-4 px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-700">
