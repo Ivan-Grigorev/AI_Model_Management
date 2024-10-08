cbf0011ea3d21f6e7fa1ce52ac38d9322dcab14a
Ivan Grigorev <iv.st.grigorev@gmail.com>
2024-10-08 08:28:22 +0200

    Add date field for timestamping entries

--- ai_model_management/backend/app/api/models.py
+++ ai_model_management/backend/app/api/models.py
@@ -1,11 +1,12 @@
 """API routes for creating, listing, and fetching specific models."""
 
 from typing import List
+from datetime import datetime
 
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
-from ..core.database import SessionLocal
+from ..core.database import get_db
 from ..models.other_models import Model
 from ..schemas.model_schemas import ModelCreate, ModelResponse
 
@@ -14,7 +15,7 @@ router = APIRouter()
 
 # Route to create a model
 @router.post('/models', response_model=ModelResponse)
-def create_model(model: ModelCreate):
+def create_model(model: ModelCreate, db: Session = Depends(get_db)):
     """
     Create a new model.
 
@@ -25,8 +26,9 @@ def create_model(model: ModelCreate):
         The created model.
     """
 
-    db: Session = SessionLocal()
-    new_model = Model(name=model.name)
+    new_model = Model(
+        name=model.name, creation_date=datetime.now().strftime('%Y/%m/%d %H:%M:%S')
+    )
     db.add(new_model)
     db.commit()
     db.refresh(new_model)
@@ -35,21 +37,21 @@ def create_model(model: ModelCreate):
 
 # Route to list all models
 @router.get('/models', response_model=List[ModelResponse])
-def list_models():
+def list_models(db: Session = Depends(get_db)):
     """
     Get all models.
 
     Returns:
         List of all models in the database.
     """
-    db: Session = SessionLocal()
+
     models = db.query(Model).all()
     return models
 
 
 # Router to get a specific model by ID
 @router.get('/models/{model_id}', response_model=ModelResponse)
-def get_model(model_id: int):
+def get_model(model_id: int, db: Session = Depends(get_db)):
     """
     Retrieve a specific model by its ID.
 
@@ -63,7 +65,6 @@ def get_model(model_id: int):
         HTTP 404 if not found.
     """
 
-    db: Session = SessionLocal()
     model = db.query(Model).filter(Model.id == model_id).first()
     if not model:
         raise HTTPException(status_code=404, detail='Model not found')
