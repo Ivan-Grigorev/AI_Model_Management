da16d37765cce12969416386e174e0e253756029
Ivan Grigorev <iv.st.grigorev@gmail.com>
2024-10-09 22:13:33 +0200

    Modify HomeView.vue

--- ai_model_management/frontend/src/views/HomeView.vue
+++ ai_model_management/frontend/src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="flex items-center justify-center min-h-screen bg-gray-100">
+  <div class="flex items-center justify-center min-h-screen bg-white">
     <div class="text-center">
-      <img alt="Vue logo" src="../assets/logo.png" class="mb-4" />
+      <img alt="Vue logo" src="../assets/logo.png" class="mb-4"/>
       <h1 class="text-3xl font-bold">Welcome to the AI Model Management App</h1>
       <p class="mt-6 mb-8 text-gray-700">To manage your AI models, please log in.</p>
       <router-link
